27259a3e30a14c6284b2dd2c025a0268c473f4f8
Andrey <g4r89@ya.ru>
2017-01-11 09:04:55 +0000

    Create chroot.sh

---
+++ chroot.sh
@@ -0,0 +1,18 @@
+ln -s /usr/share/zoneinfo/Europe/Moscow /etc/localtime
+hwclock --systohc --utc
+
+sed -i '/#en_US.UTF/s/#//' /etc/locale.gen
+locale-gen
+
+echo g4xz > /etc/hostname
+
+systemctl enable dhcpcd
+
+pacman -Sy grub btrfs-progs
+grub-install --target=i386-pc --recheck /dev/sda
+grub-mkconfig -o /boot/grub/grub.cfg
+
+pacman -S xorg-server xorg-xinit i3-wm i3status dmenu chromium
+
+exit
+umount -R /mnt
