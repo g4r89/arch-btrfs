9cb52f6a745e6fa717813f847295d0b7c14a7f1b
Andrey <g4r89@ya.ru>
2017-01-11 09:06:21 +0000

    Update chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -8,6 +8,9 @@ echo g4xz > /etc/hostname
 
 systemctl enable dhcpcd
 
+useradd -m andrey
+passwd andrey
+
 pacman -Sy grub btrfs-progs
 grub-install --target=i386-pc --recheck /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
